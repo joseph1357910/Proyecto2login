706f005f2fc7aa58024bf968beb93d6445cfa313
DELL <DELL@JOSEPH>
2023-12-10 09:38:01 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto2login/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto2login
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tcorreo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tclave_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bagregar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
